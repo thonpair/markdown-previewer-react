94e263a7c2529b52750c429cd5376c005ad1487a
Emmanuel HEMERIT <emmanuel@hemer.it>
2023-03-31 04:53:44 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import MarkdowPreviewer from './MarkdownPreviewer';
+
+function App() {
+  return (
+    <div className="App">
+    <MarkdowPreviewer />      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/MarkdownPreviewer.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Markdown from 'marked-react';
+import './MarkdownPreviewer.css';
+
+export default class MardowPreviewer extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            text:
+            "a\n# h1 Heading\n## h2 Heading\n[link text](https://emmanuel.hemer.it)\nInline `code`\n```\nSample text here...\n```\n+ list item 1\n+ list item 2\n> Blockquote\n![Stormtroopocat](https://octodex.github.com/images/stormtroopocat.jpg \"The Stormtroopocat\")\n**This is bold text**"
+            , preview: ''}
+    }
+
+    handleChange = (e) => {
+        e.preventDefault();
+        this.setState({text: e.target.value})
+    }
+
+    render(){
+        return(
+            <div id="container">
+                <h1 id="title">Markdown Previewer</h1>
+                <textarea id="editor" onChange={this.handleChange} value={this.state.text}></textarea>
+                <div id="preview"><Markdown>{this.state.text}</Markdown></div>
+            </div>
+        )
+    }
+}
